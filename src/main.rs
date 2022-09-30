3d507b0e24394e2772b6072ac6ab7c879b980fba
Simon Gauvin <gauvinsimon@gmail.com>
2022-09-29 20:36:50 -0400

    Add cpal and print pulse audio device

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+
 fn main() {
-    println!("Hello, world!");
+    let host = cpal::default_host();
+    let devices = host.devices().unwrap();
+
+    for device in devices {
+        let supported_input_configs = device.supported_input_configs();
+        if supported_input_configs.is_err() {
+            continue;
+        }
+
+        let supported_input_configs = supported_input_configs.unwrap().next();
+        if supported_input_configs.is_none() {
+            continue;
+        }
+
+        let config = supported_input_configs.unwrap().with_max_sample_rate();
+
+        if device.name().unwrap() == "pulse" {
+            println!("Device: {}, config: {:?}", device.name().unwrap(), config);
+            break;
+        }
+    }
 }
