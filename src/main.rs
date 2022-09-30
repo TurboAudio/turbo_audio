5b5aacd79ded7d7ba7a78ff014f820c5b5da05bc
Simon Gauvin <gauvinsimon@gmail.com>
2022-09-30 16:15:34 -0400

    Read audio stream from pulse

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,53 @@
 use cpal::traits::{DeviceTrait, HostTrait};
+// use cpal::Data;
+
+fn get_audio_device() -> Option<cpal::Device> {
+    let host = cpal::default_host();
+    let mut devices = host.devices().expect("Host has no audio device");
+    devices.find(|device| device.name().expect("Audio device has no name") == "pulse")
+}
+
+fn get_input_config(audio_device: &cpal::Device) -> cpal::SupportedStreamConfig {
+    audio_device
+        .supported_input_configs()
+        .expect("Device has no supported input configs")
+        .next()
+        .expect("Device has no supported input configs")
+        .with_sample_rate(cpal::SampleRate(44100))
+}
+
+fn read_audio_stream<T: cpal::Sample + std::fmt::Display>(data: &[T], _: &cpal::InputCallbackInfo) {
+    for point in data {
+        println!("{}", point.to_f32());
+    }
+}
 
 fn main() {
-    let host = cpal::default_host();
-    let devices = host.devices().unwrap();
+    let audio_device = get_audio_device().expect("No suitable audio device found");
+    let input_config = get_input_config(&audio_device);
+    let sample_format = input_config.sample_format();
+    let config = input_config.into();
 
-    for device in devices {
-        let supported_input_configs = device.supported_input_configs();
-        if supported_input_configs.is_err() {
-            continue;
+    println!("Sample format: {:?}", sample_format);
+
+    let err_fn = |err| {
+        panic!("ERROR: {:?}", err);
+    };
+
+    let _stream = match sample_format {
+        cpal::SampleFormat::U16 => {
+            audio_device.build_input_stream(&config, read_audio_stream::<u16>, err_fn)
         }
-
-        let supported_input_configs = supported_input_configs.unwrap().next();
-        if supported_input_configs.is_none() {
-            continue;
+        cpal::SampleFormat::I16 => {
+            audio_device.build_input_stream(&config, read_audio_stream::<i16>, err_fn)
         }
-
-        let config = supported_input_configs.unwrap().with_max_sample_rate();
-
-        if device.name().unwrap() == "pulse" {
-            println!("Device: {}, config: {:?}", device.name().unwrap(), config);
-            break;
+        cpal::SampleFormat::F32 => {
+            audio_device.build_input_stream(&config, read_audio_stream::<f32>, err_fn)
         }
+    }
+    .unwrap();
+
+    loop {
+        std::thread::sleep(std::time::Duration::new(5, 0));
     }
 }
