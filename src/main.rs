65cff21424d36bc3b4d4f5e66b7149bfd7f39242
Simon Gauvin <gauvinsimon@gmail.com>
2022-10-05 22:00:15 -0400

    Add command line arguments to select host, audio device, and sample rate

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 mod audio;
 use audio::start_audio_loop;
+use clap::Parser;
+
+/// Haha brr
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the input audio device to choose
+    #[arg(long)]
+    device_name: Option<String>,
+
+    /// Toggle if Jack should be used as the audio host
+    #[arg(long, default_value_t = false, action = clap::ArgAction::SetTrue)]
+    jack: bool,
+
+    /// Sample rate of the input stream
+    #[arg(long, default_value_t = 48000)]
+    sample_rate: u32,
+}
 
 fn main() {
-    let (_stream, rx) = start_audio_loop();
+    let args = Args::parse();
+    let (_stream, rx) = start_audio_loop(args.device_name, args.jack, args.sample_rate);
 
     loop {
         for patnais in &rx {
