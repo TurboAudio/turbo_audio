2268618626c5e062f72ec0f0c9ffcbb70143c8bf
samichoulo911 <samer.massaad@gmail.com>
2022-12-05 23:14:47 -0500

    Match on tuple to remove additional cast call

--- src/resources/effects.rs
+++ src/resources/effects.rs
@@ -3,7 +3,17 @@ use self::{moody::Moody, raindrop::Raindrops};
 pub mod moody;
 pub mod raindrop;
 
+
 pub enum Effect {
     Moody(Moody),
     Raindrop(Raindrops),
+}
+
+impl Effect {
+    pub fn settings_id(&self) -> &i32 {
+        match self {
+            Effect::Moody(moody) => &moody.settings_id,
+            Effect::Raindrop(raindrop) => &raindrop.settings_id,
+        }
+    }
 }
