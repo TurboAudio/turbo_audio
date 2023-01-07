955e7552ed28378dffc397ad292f73dc8ac60e10
samichoulo911 <43357319+samichoulo911@users.noreply.github.com>
2023-01-07 14:32:39 -0500

    Use custom error types for Tcp logic (#20)
    * Consider the computation time of the loop functions
    
    * Use chrono crate to get negative durations
    
    * Remove unused variable
    
    * Fix clippy errors
    
    * Format
    
    * Make data_queue private
    
    * Remove the use of anyhow for more explicit error typing
    
    * Split connection attempt errors and connection errors
    
    * Format
    
    * Add comments to TCP logic
    
    * Discard overriden ring buffer value
    
    * Format

--- src/connections/tcp.rs
+++ src/connections/tcp.rs
@@ -1,4 +1,3 @@
-use anyhow::anyhow;
 use ring_channel::*;
 use std::{
     io::Write,
@@ -9,8 +8,18 @@ use std::{
 };
 
 pub struct TcpConnection {
-    pub data_queue: ring_channel::RingSender<Vec<u8>>,
-    connection_thread: Option<JoinHandle<anyhow::Result<()>>>,
+    data_queue: ring_channel::RingSender<Vec<u8>>,
+    connection_thread: Option<JoinHandle<Result<(), TcpConnectionError>>>,
+}
+
+enum TcpConnectionError {
+    ConnectionFailed(ConnectionAttemptError),
+    UnableToReconnect(ConnectionAttemptError, std::io::Error),
+}
+
+enum ConnectionAttemptError {
+    Unreachable(std::net::SocketAddr),
+    ConfigurationFailed(std::io::Error),
 }
 
 impl TcpConnection {
@@ -22,31 +31,52 @@ impl TcpConnection {
         }
     }
 
+    pub fn send_data(&mut self, packet: Vec<u8>) -> Result<(), SendError<Vec<u8>>> {
+        self.data_queue.send(packet).map(|_| ())
+    }
+
     fn start_connection_thread(
         ip: std::net::SocketAddr,
     ) -> (
         ring_channel::RingSender<Vec<u8>>,
-        JoinHandle<anyhow::Result<()>>,
+        JoinHandle<Result<(), TcpConnectionError>>,
     ) {
         let buffer_size: NonZeroUsize = NonZeroUsize::new(64).unwrap();
         let (tx, mut rx) = ring_channel::<Vec<u8>>(buffer_size);
-        let connection_thread = thread::spawn(move || -> Result<(), anyhow::Error> {
-            loop {
-                let mut connection = TcpConnection::attempt_connection(ip, None, None)?;
-
+        let connection_thread =
+            thread::spawn(move || -> Result<(), TcpConnectionError> {
+                let mut disconnect_error = None;
+                // This loop essures we keep reconnecting if possible
                 loop {
-                    match rx.recv() {
-                        Ok(data) => {
-                            if let Err(e) = connection.write_all(&data) {
-                                eprintln!("Failed to send packet to connection. Error: {:?}", e);
-                                break;
+                    let mut connection = TcpConnection::attempt_connection(ip, None, None)
+                        .map_err(|attempt_error| match disconnect_error {
+                            Some(disconnect_error) => {
+                                // This error comes from the last disconnect
+                                TcpConnectionError::UnableToReconnect(
+                                    attempt_error,
+                                    disconnect_error,
+                                )
                             }
+                            None => TcpConnectionError::ConnectionFailed(attempt_error),
+                        })?;
+
+                    // This loop sends the packets in data_queue through the TCP socket
+                    loop {
+                        match rx.recv() {
+                            Ok(data) => {
+                                if let Err(e) = connection.write_all(&data) {
+                                    disconnect_error = Some(e);
+                                    // We break from this loop to allow reconnection to happen
+                                    break;
+                                }
+                            }
+                            // If an error occurs, the data_queue has no more sender
+                            // and meaning the thread can exit correctly
+                            Err(_) => return Ok(()),
                         }
-                        Err(_) => return Ok(()),
                     }
                 }
-            }
-        });
+            });
 
         (tx, connection_thread)
     }
@@ -55,7 +85,7 @@ impl TcpConnection {
         ip: std::net::SocketAddr,
         max_connection_attempts: Option<i32>,
         connection_timeout: Option<Duration>,
-    ) -> anyhow::Result<TcpStream> {
+    ) -> Result<TcpStream, ConnectionAttemptError> {
         let max_connection_attempts = max_connection_attempts.unwrap_or(20);
         let connection_timeout = connection_timeout.unwrap_or(Duration::from_secs(3));
         for _ in 0..max_connection_attempts {
@@ -64,13 +94,13 @@ impl TcpConnection {
                 Ok(stream) => {
                     stream
                         .set_write_timeout(Some(Duration::from_millis(100)))
-                        .map_err(|_| anyhow!("Unable to set write timeout on {:?}", ip))?;
+                        .map_err(ConnectionAttemptError::ConfigurationFailed)?;
                     return Ok(stream);
                 }
                 Err(_) => continue,
             }
         }
-        Err(anyhow!("Unable to connect to {:?}", ip))
+        Err(ConnectionAttemptError::Unreachable(ip))
     }
 }
 
