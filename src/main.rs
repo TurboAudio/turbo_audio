c0cbec69bfccfb5f2ad31fc66119d0c3abd43d7b
samichoulo911 <samer.massaad@gmail.com>
2023-01-22 01:01:56 -0500

    Remove commented code

--- src/main.rs
+++ src/main.rs
@@ -49,35 +49,8 @@ enum RunLoopError {
 }
 
 fn test_and_run_loop(mut audio_processor: AudioSignalProcessor) -> Result<(), RunLoopError> {
-    // let mut settings: HashMap<usize, Settings> = HashMap::new();
-    // let mut effects: HashMap<usize, Effect> = HashMap::new();
-    // let mut effect_settings: HashMap<usize, usize> = HashMap::new();
-    // let mut connections: HashMap<usize, Connection> = HashMap::new();
-    // let mut ledstrips = Vec::new();
-    // let mut lua_effects_registry: HashMap<String, usize> = HashMap::default();
     let mut controller = Controller::new();
 
-
-    // let moody_id = register_effect(
-    //     Effect::Moody(moody),
-    //     &mut effects,
-    //     &mut lua_effects_registry,
-    // );
-    // effect_settings.insert(moody_id, 0);
-
-    // let raindrops_id = register_effect(
-    //     Effect::Raindrop(raindrop),
-    //     &mut effects,
-    //     &mut lua_effects_registry,
-    // );
-    // effect_settings.insert(raindrops_id, 1);
-
-
-    // let lua_id = register_effect(
-    //     Effect::Lua(lua_effect),
-    //     &mut effects,
-    //     &mut lua_effects_registry,
-    // );
     let lua_id: usize = 1;
     let lua_effect = LuaEffect::new("scripts/sketchers.lua", &audio_processor).map_err(|e| {
         log::error!("{:?}", e);
@@ -130,16 +103,12 @@ fn test_and_run_loop(mut audio_processor: AudioSignalProcessor) -> Result<(), Ru
     controller.add_led_strip(led_strip_id, ls1);
     controller.link_led_strip_to_connection(led_strip_id, connection_id);
 
-    // controller.link_led_strip_to_connection(led_strip_id, connection_id)
-    // ls1.connection_id = Some(connection_id);
-    // ledstrips.push(ls1);
-
     if let Err(e) = start_hot_reload_lua_effects() {
         log::error!("Hot reload may not be active: {e:?}");
     }
-
-
     let (hot_reload_rx, _debouncer) = start_hot_reload_lua_effects().unwrap();
+
+
     let mut lag = chrono::Duration::zero();
     let duration_per_tick: chrono::Duration = chrono::Duration::seconds(1) / 60;
     let mut last_loop_start = std::time::Instant::now();
@@ -158,9 +127,6 @@ fn test_and_run_loop(mut audio_processor: AudioSignalProcessor) -> Result<(), Ru
         let _fft_result_read_lock = audio_processor.fft_result.read().unwrap();
         controller.update_led_strips();
         let _ = controller.send_ledstrip_colors();
-        // let _update_result =
-        //     update_ledstrips(&mut ledstrips, &mut effects, &effect_settings, &settings);
-        // let _send_result = send_ledstrip_colors(&mut ledstrips, &mut connections);
 
         check_lua_files_changed(
             &hot_reload_rx,
@@ -172,78 +138,6 @@ fn test_and_run_loop(mut audio_processor: AudioSignalProcessor) -> Result<(), Ru
         lag = lag.checked_sub(&duration_per_tick).unwrap();
     }
 }
-
-// fn update_ledstrips(
-//     ledstrips: &mut [LedStrip],
-//     effects: &mut HashMap<usize, Effect>,
-//     effect_settings: &HashMap<usize, usize>,
-//     settings: &HashMap<usize, Settings>,
-// ) -> anyhow::Result<()> {
-//     for ledstrip in ledstrips {
-//         for (effect_id, interval) in &ledstrip.effects {
-//             let leds = ledstrip
-//                 .colors
-//                 .get_mut(interval.0..=interval.1)
-//                 .ok_or_else(|| anyhow!("Ledstrip interval out of bounds"))?;
-//             let effect = effects
-//                 .get_mut(effect_id)
-//                 .ok_or_else(|| anyhow!("Effect id was not found."))?;
-//             let setting_id = effect_settings
-//                 .get(effect_id)
-//                 .ok_or_else(|| anyhow!("Setting id not found"))?;
-//             let setting = settings.get(setting_id);
-//             match (effect, setting) {
-//                 (Effect::Moody(_moody), Some(Settings::Moody(settings))) => {
-//                     update_moody(leds, settings);
-//                 }
-//                 (Effect::Raindrop(raindrop), Some(Settings::Raindrop(settings))) => {
-//                     update_raindrop(leds, settings, &mut raindrop.state);
-//                 }
-//                 (Effect::Lua(lua), Some(Settings::Lua(settings))) => {
-//                     if let Err(e) = lua.tick(leds, settings) {
-//                         log::error!("Error when executing lua function: {:?}", e);
-//                     }
-//                 }
-//                 _ => panic!("Effect doesn't match settings"),
-//             }
-//         }
-//     }
-//     Ok(())
-// }
-//
-// fn send_ledstrip_colors(
-//     ledstrips: &mut Vec<LedStrip>,
-//     connections: &mut HashMap<usize, Connection>,
-//     ledstrip_connections: &mut HashMap<usize, usize>,
-//     ledstrip_id: usize,
-// ) -> anyhow::Result<()> {
-//     for ledstrip in ledstrips {
-//         if let Some(connection_id) = ledstrip_connections.get(&ledstrip_id) {
-//             if let Some(connection) = connections.get(connection_id) {
-//                 let data = ledstrip
-//                     .colors
-//                     .iter()
-//                     .flat_map(|color| color.to_bytes())
-//                     .collect::<Vec<_>>();
-//                 match connection {
-//                     Connection::Tcp(tcp_connection) => {
-//                         // If send fails, connection is closed.
-//                         if let Err(error) = tcp_connection.send_data(data) {
-//                             log::error!("{:?}", error);
-//                             connections.remove(&connection_id);
-//                             ledstrip_connections.remove(&ledstrip_id);
-//                         }
-//                         return Ok(());
-//                     }
-//                     Connection::Usb(_terminal) => {
-//                         todo!("Implement Usb connection");
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//     Ok(())
-// }
 
 fn main() -> Result<(), RunLoopError> {
     pretty_env_logger::init();
